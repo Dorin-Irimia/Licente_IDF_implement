597be4e0322455f9e14ebec50d646176c8f916b6
Irimia Dorin <42108589+Dorin-Irimia@users.noreply.github.com>
2023-02-02 13:24:10 +0200

    remove COPYRIGHT v0.2

--- ViTAL_L1_L13/ViTAL_BSW_Complete/components/ViTAL/include/BSW/HAL/Servo_Motor/servo_motor.h
+++ ViTAL_L1_L13/ViTAL_BSW_Complete/components/ViTAL/include/BSW/HAL/Servo_Motor/servo_motor.h
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * COPYRIGHT (C) VITESCO TECHNOLOGIES
- * ALL RIGHTS RESERVED.
- *
- * The reproduction, transmission or use of this document or its
- * contents is not permitted without express written authority.
- * Offenders will be liable for damages. All rights, including rights
- * created by patent grant or registration of a utility model or design,
- * are reserved.
- ******************************************************************************/
 
 #ifndef COMPONENTS_VITAL_BSW_HAL_SERVO_MOTOR_H
 #define COMPONENTS_VITAL_BSW_HAL_SERVO_MOTOR_H
