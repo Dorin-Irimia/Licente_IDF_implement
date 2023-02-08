f651de0ded24d7bbb9b279fd4351bd3a8d651cc2
Irimia Dorin <42108589+Dorin-Irimia@users.noreply.github.com>
2023-02-08 11:31:32 +0200

    servo

--- ViTAL_L1_L13/ViTAL_BSW_Complete/components/ViTAL/include/BSW/HAL/Servo_Motor/servo_motor.h
+++ ViTAL_L1_L13/ViTAL_BSW_Complete/components/ViTAL/include/BSW/HAL/Servo_Motor/servo_motor.h
@@ -6,8 +6,8 @@
 
 void SERVO_vChangeAngle(uint32_t u32ServoAngle);
 
-#define SERVOMOTOR_CW_DC 1400
-#define SERVOMOTOR_STOP_DC 1500
-#define SERVOMOTOR_CCW_DC 1600
+#define SERVOMOTOR_CW_DC 1343 //standard value is 1400
+#define SERVOMOTOR_STOP_DC 3662 //standard value is 1500
+#define SERVOMOTOR_CCW_DC 5371 //standard value is 1600
 
 #endif
